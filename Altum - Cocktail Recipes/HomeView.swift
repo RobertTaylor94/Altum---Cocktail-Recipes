695c288b0bb4416c1b36c98449cba2a21544c532
RobertTaylor94 <rjt903@gmail.com>
2021-05-05 19:22:30 +0100

    Updated HomeView
    Correctly displays user library of drinks
    Displays one random drink when view is refreshed(Bug: On initial loading of app more than one drink is displayed)

--- Altum - Cocktail Recipes/HomeView.swift
+++ Altum - Cocktail Recipes/HomeView.swift
@@ -6,8 +6,15 @@
 //
 
 import SwiftUI
+import RealmSwift
 
 struct HomeView: View {
+    
+    @ObservedObject var networkManager = NetworkManager(search: "")
+    @ObservedObject var randomDrinkManager = RandomDrinkManager()
+    @State private var drinks: Results<DBDrink>?
+    let realm = try! Realm()
+    
     var body: some View {
 
         NavigationView {
@@ -16,22 +23,27 @@ struct HomeView: View {
             VStack(alignment: .leading, spacing: 10) {
                     Text("My Drinks")
                         .font(.title)
-                    ScrollView(.horizontal) {
+                ScrollView(.horizontal, showsIndicators: false) {
                         HStack(spacing: 20) {
                             
-                            VStack {
-                                SavedCocktailView()
-                                NavigationLink(
-                                    destination: CocktailDetailView(),
-                                    label: {
-                                        Text("Margarita")
-                                            .padding()
-                                            .background(Color("Foreground"))
-                                            .cornerRadius(10)
-                                            .foregroundColor(Color("Background"))
-                                    })
+                            if drinks != nil {
+                                ForEach(drinks!) { i in
+                                    VStack {
+                                        SavedCocktailView(imageUrl: i.strDrinkThumb)
+                                        NavigationLink(
+                                            destination: CocktailDetailView(strDrink: i.strDrink),
+                                            label: {
+                                                Text(i.strDrink)
+                                                    .padding()
+                                                    .background(Color("Foreground"))
+                                                    .cornerRadius(10)
+                                                    .foregroundColor(Color("Background"))
+                                            })
+                                    }
+                                }
+                            } else {
+                                
                             }
-                            
                         }
                     }
                 }
@@ -39,16 +51,20 @@ struct HomeView: View {
                 .background(Image("neon"))
                 .foregroundColor(.white)
                 .cornerRadius(20)
+                .onAppear(){loadDrinks()}
                 
                 Spacer()
-                
+            ForEach(randomDrinkManager.randomDrink) { i in
                 HStack {
-                    RandomDrinkView()
+                    RandomDrinkView(title: i.strDrink, ing1: i.strIngredient1 ?? "", ing2: i.strIngredient2 ?? "", ing3: i.strIngredient3 ?? "", ing4: i.strIngredient4 ?? "", ing5: i.strIngredient5 ?? "", thumb: i.strDrinkThumb, instructions: i.strInstructions)
+                    
                 }
                 .padding()
                 .background(Color.black)
                 .foregroundColor(.white)
                 .cornerRadius(20)
+            }
+                
                 
                 Spacer()
                 Spacer()
@@ -56,10 +72,15 @@ struct HomeView: View {
             }
             .padding()
             .background(Color("Background")).ignoresSafeArea()
+            .onAppear() {self.randomDrinkManager.getRandomDrink()}
         }
     }
    
+    func loadDrinks() {
+        drinks = realm.objects(DBDrink.self)
     }
+    
+}
 
 
 struct ContentView_Previews: PreviewProvider {
