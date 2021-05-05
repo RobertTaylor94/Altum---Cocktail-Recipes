7c5b8583b64dd0eb35ac4092d428b2ebb0577ea5
RobertTaylor94 <rjt903@gmail.com>
2021-05-05 19:20:09 +0100

    Allows DetailView to display the details from the selected Drink

--- Altum - Cocktail Recipes/CocktailDetailView.swift
+++ Altum - Cocktail Recipes/CocktailDetailView.swift
@@ -8,8 +8,28 @@
 import SwiftUI
 
 struct CocktailDetailView: View {
+    
+    @State var strDrink: String = ""
+    @State var strGlass: String = ""
+    @State var strInstructions: String = ""
+    @State var strDrinkThumb: String = ""
+    @State var strIngredient1: String = ""
+    @State var strIngredient2: String = ""
+    @State var strIngredient3: String = ""
+    @State var strIngredient4: String = ""
+    @State var strIngredient5: String = ""
+    @State var strMeasure1: String = ""
+    @State var strMeasure2: String = ""
+    @State var strMeasure3: String = ""
+    @State var strMeasure4: String = ""
+    @State var strMeasure5: String = ""
+    
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+        Text(strDrink)
+        
+        
     }
 }
 
